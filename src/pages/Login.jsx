6c45e09645fd9f4f9e9ff7fadaeaec3192d9d70b
stack6649-arch <stack6649@gmail.com>
2026-04-11 16:43:46 +0300

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -186,7 +186,7 @@ function LoginSuccessModal({ username, open, onClose }) {
   );
 }
 
-const API_URL = "https://stacksapp-backend-main.onrender.com";
+const API_URL = "https://keymuse-backend.onrender.com";
 
 export default function Login({ refreshRecords }) {
   const [input, setInput] = useState("");
